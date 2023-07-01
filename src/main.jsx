265c9b4240e372650b1b6219c080c6a0bf528f02
Abigail Sarzuri <abigailsarzuri@gmail.com>
2023-07-01 17:25:51 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import App from './App';
+import './index.css';
+import { store, persistor } from './redux/store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
